85a36fe3c2cefc4b0fc29f88389b0165cf04bfb3
kyposani@outlook.com <kyposani@outlook.com>
2019-12-24 16:38:00 +0300

    added Reverse() method

--- New Solution/HW.06_Task3/Program.cs
+++ New Solution/HW.06_Task3/Program.cs
@@ -17,6 +17,8 @@ namespace HW._06_Task3
             long index = 0;
             long last = array.Length - 1;
             Stopwatch timer = new Stopwatch();
+            Console.WriteLine("Tap any key to test myReverse Method");
+            Console.ReadKey();
             timer.Start();
             while (index != last && index < array.Length / 2)
             {
@@ -28,7 +30,29 @@ namespace HW._06_Task3
             }
             timer.Stop();
             TimeSpan ts = timer.Elapsed;
+            Console.WriteLine("Operation speed of myReverse Method is:");
             Console.WriteLine("{0}h:{1}m:{2}s.{3}ms", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+            Console.WriteLine("Tap any key to continue");
+            Console.ReadKey();
+            long[] array2 = new long[100_000_000];
+            Random rnd2 = new Random();
+            for(long j = 0; j < array2.Length; j++)
+            {
+                array2[j] = rnd2.Next();
+            }
+            Stopwatch timer2 = new Stopwatch();
+            Console.WriteLine("Tap any key to test MS Reverse Method");
+            Console.ReadKey();
+            timer2.Start();
+            Array.Reverse(array2);
+            timer2.Stop();
+            TimeSpan ts2 = timer2.Elapsed;
+            Console.WriteLine("Operation speed of MS Reverse Method is:");
+            Console.WriteLine("{0}h:{1}m:{2}s.{3}ms", ts2.Hours, ts2.Minutes, ts2.Seconds, ts2.Milliseconds);
+            Console.ReadKey();
+
+
         }
+
     }
 }
