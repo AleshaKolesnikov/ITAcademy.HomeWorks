e89b6d64a3b390c885088de5d349dcde7719ca9f
kyposani@outlook.com <kyposani@outlook.com>
2019-12-29 21:24:30 +0300

    TASK1 - completed

---
+++ New Solution/HW.07_Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW._07_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стихотворение в одну строку, разделяя каждый переход на новую строчку символом \";\"");
+            string poem = Console.ReadLine();
+            Console.WriteLine("Нажмите любую клавишу для замены \"O\" на \"A\" ");
+            Console.ReadKey();
+            char[] splitter = { ';' };
+            poem = poem.Replace("о", "а");
+            poem = poem.Replace("О", "А");
+            poem = poem.Replace("o", "a");
+            poem = poem.Replace("O", "A");
+            string[] poemForString = poem.Split(splitter, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string i in poemForString)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
