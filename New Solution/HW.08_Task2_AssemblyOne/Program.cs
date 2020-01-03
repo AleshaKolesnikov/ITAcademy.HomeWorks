79cf8fe1943fad372edbcb36f605a416d439ece7
kyposani@outlook.com <kyposani@outlook.com>
2020-01-03 19:17:47 +0300

    created 2 new projects for task2

---
+++ New Solution/HW.08_Task2_AssemblyOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW._08_Task2_AssemblyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
