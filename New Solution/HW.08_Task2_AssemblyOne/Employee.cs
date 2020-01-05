60b8bb2c81f406a1f37f647985608ebb648c04f1
kyposani@outlook.com <kyposani@outlook.com>
2020-01-05 22:22:52 +0300

    added examples of acces modifiers in assembly one

--- New Solution/HW.08_Task2_AssemblyOne/Employee.cs
+++ New Solution/HW.08_Task2_AssemblyOne/Employee.cs
@@ -6,5 +6,30 @@ namespace HW._08_Task2_AssemblyOne
 {
     class Employee
     {
+        //working with consts
+        public const int publicBirthDate = 1993;
+        private const int privateBirthDate = 1994;
+        protected const int protectedBirthDate = 1995;
+        internal const int internalBirthDate = 1996;
+        protected internal int piBirthDate = 1997;
+        private protected int ppBirthDate = 1998;
+        //working with variables
+        public string publicName = "public";
+        private string privateName = "private";
+        protected string protectedName = "protected";
+        internal string internalName = "internal";
+        protected internal string piName = "protected internal";
+        private protected string ppName = "private protected";
+        //working with method
+        private void privateMethod()
+        {
+            Console.WriteLine("Hi");
+        }
+        public void publicMethod() { }
+        internal void internalMethod() { }
+        protected void protectedMethod() { }
+        protected internal void piMethod() { }
+        private protected void ppMethod() { }
+
     }
 }
--- New Solution/HW.08_Task2_AssemblyOne/Program.cs
+++ New Solution/HW.08_Task2_AssemblyOne/Program.cs
@@ -6,7 +6,32 @@ namespace HW._08_Task2_AssemblyOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee empl = new Employee();
+
+            int publicConst = Employee.publicBirthDate;           //Можно использовать, т.к. public дает доступ из любой сборки и из места в коде
+            int internalConst = Employee.internalBirthDate;       //Можно использовать, т.к. internal позволяет использовать переменную из 
+            //                                                      любой части кода, однако не из другой сборки, либо программы;
+            //int privateConst = Employee.privateBirthDate;         Нельзя использовать модификатор private вне используемого класса
+            //int protectedConst = Employee.protectedBirthDate;     Нельзя использовать модификатор protected вне используемого класса,
+            //                                                      либо класса наследника (независимо от сборки)
+            // !!!!!!  int piConst = Employee.piBirthDate;          Странно, что константы необходимо вызывать по имени класса, а здесь
+            int piConst = empl.piBirthDate;                       //модификатор protected internal позволяет работать только с объектом класса.
+            //int ppConst = Employee.ppBirthDate;                   Нельзя использовать модификатор private protected вне производного либо текущего
+            //                                                      класса вне сборки
+            
+            string publicExample = empl.publicName;               // модификатор public позволяет использовать переменную из любого места из любой сборки
+            string internalExample = empl.internalName;           //класс Employee находится в той же сборке, что и используемый класс Program
+            string piExample = empl.piName;                       //класс Employee находится в той же сборке, что и класс Program
+            //string privateExample = empl.privateName;             privateName находится за пределами класса;
+            //string protectedExample = empl.protectedName;         класс Program не является дочерним для класса Employee;
+            //string ppExample = empl.ppName;                       класс Program не наследуется от класса Employee, хоть и находится в той же сборке  
+
+            empl.piMethod();
+            empl.publicMethod();
+            empl.internalMethod();
+            //empl.privateMethod();
+            //empl.protectedMethod();
+            //empl.ppMethod();
         }
     }
 }
