bd96bdbefe1e9c1181954d53bcd4c2ff266d9018
kyposani@outlook.com <kyposani@outlook.com>
2020-01-19 15:07:04 +0300

    modified link in task 2

--- New Solution/HW.10/CodeWars/Person.cs
+++ New Solution/HW.10/CodeWars/Person.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace HW._10.CodeWars
 {
-    // Link - https://www.codewars.com/kata/55a144eff5124e546400005a/solutions/csharp
+    // Link - https://www.codewars.com/kata/55a144eff5124e546400005a
 
     //Your task is to complete this Class, the Person class has been created. You must fill in the Constructor 
     //method to accept a name as string and an age as number,complete the get Info property
