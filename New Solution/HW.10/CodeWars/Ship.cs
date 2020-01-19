8f02687021154b45760ea62df4cce520b655e9f6
kyposani@outlook.com <kyposani@outlook.com>
2020-01-19 14:26:03 +0300

    First task completed

---
+++ New Solution/HW.10/CodeWars/Ship.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW._10.CodeWars
+{
+    //Link - https://www.codewars.com/kata/54fe05c4762e2e3047000add/train/csharp
+    //You are a leader of a small pirate crew. And you have a plan. 
+    //With the help of OOP you wish to make a pretty efficient system to identify ships with a heavy booty on board.
+    public class Ship
+    {
+        public int Draft;
+        public int Crew;
+
+        public Ship(int draft, int crew)
+        {
+            Draft = draft;
+            Crew = crew;
+        }
+        public bool IsWorthIt()
+        {
+            if (Draft-Crew*1.5 >= 20)
+            return true;
+            else return false;
+        }
+    }
+}
---
+++ New Solution/HW.10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HW._10.CodeWars;
+
+namespace HW._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndA = new Random();
+            Random rndB = new Random();
+            int draft = rndA.Next();
+            int crew = rndB.Next();
+
+            Ship ship = new Ship(draft, crew);
+            Console.WriteLine(ship.IsWorthIt());
+
+        }
+    }
+}
