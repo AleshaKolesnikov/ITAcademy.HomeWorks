0b75f68523c8fa1815c8a0d5b4b73d52da373362
kyposani@outlook.com <kyposani@outlook.com>
2020-01-29 18:19:37 +0300

    added logs

--- New Solution/HW.11/Program.cs
+++ New Solution/HW.11/Program.cs
@@ -7,6 +7,9 @@ namespace HW._11
     {
         static void Main(string[] args)
         {
+            Logger.InitLogger();
+            Logger.Log.Info("START");
+
             BasicFigure[] figures = new BasicFigure[3];
 
             //creates random figure with random side sizes in array
@@ -19,21 +22,26 @@ namespace HW._11
                 if (rndFigure.Equals(1))
                 {
                     figures[i] = new Square("Square", rndSize);
+                    Logger.Log.Info("Created SQUARE");
                 }
                 else if (rndFigure.Equals(2))
                 {
                     figures[i] = new Circle("Circle", rndSize);
+                    Logger.Log.Info("Created CIRCLE");
                 }
                 else
                 {
                     figures[i] = new Triangle("Triangle1", rndSize);
+                    Logger.Log.Info("Created TRIANGLE");
                 }
             }
-            
+
+            Logger.Log.Info("OUTPUT");
             for (int i = 0; i < figures.Length; i++)
             {
                 Console.WriteLine($"This is {figures[i].Name}, CLR Type is {figures[i]}, Area = {figures[i].GetArea()}");
             }
+            Logger.Log.Info("FINISH \n");
         }
     }
 }
