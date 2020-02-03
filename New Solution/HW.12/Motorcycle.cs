34dc0e26ccc1bf96bc6f9130d2de1ae064738dcc
Lex Kolesnikov <kyposani@outlook.com>
2020-02-03 20:26:29 +0300

    created the layout

---
+++ New Solution/HW.12/Motorcycle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW._12
+{
+    class Motorcycle
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Model { get; set; }
+        int Year { get; set; }
+        int Odometer { get; set; }
+
+        public static Motorcycle CreateMotorcycle(int ID, string name, string model, int year, int odometer)
+        {
+            Motorcycle moto = new Motorcycle();
+            moto.Id = ID;
+            moto.Name = name;
+            moto.Model = model;
+            moto.Year = year;
+            moto.Odometer = odometer;
+            return moto;
+        }
+
+        public static Motorcycle DeleteMotorcycle(ref Motorcycle moto)
+        {
+            moto = null;
+            return moto;
+        }
+
+        public static void GetMotorcycles(Motorcycle moto)
+        {
+            if (moto != null)
+            {
+                Console.WriteLine($"ID - {moto.Id};\nName - {moto.Name};\n" +
+                    $"Model - {moto.Model};\nYear - {moto.Year};\nOdometer - {moto.Odometer}\n");
+            }
+            else Console.WriteLine("Motorcycle not set or deleted\n");
+        }
+
+        public static Motorcycle UpdateMotorcycle(Motorcycle moto, int odometer)
+        {
+            moto.Odometer = odometer;
+            return moto;
+        }
+
+        public static void GetMotorcycleById(Motorcycle[] motos, int ID)
+        {
+            Motorcycle comparedMoto = null;
+            for (int i = 0; i < motos.Length; i++)
+            {
+                if (motos[i] != null && motos[i].Id == ID)
+                {
+                    comparedMoto = motos[i];
+                    GetMotorcycles(motos[i]);
+                }
+            }
+            if (comparedMoto == null)
+                Console.WriteLine("Not found. Perhaps, specified ID don't exist");
+        }
+    }
+}
---
+++ New Solution/HW.12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HW._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motorcycle[] motorcycles = new Motorcycle[5];
+            motorcycles[0] = Motorcycle.CreateMotorcycle(1, "Honda", "RX", 2010, 35_000);
+            motorcycles[1] = Motorcycle.CreateMotorcycle(2, "BMW", "XDW", 2015, 12_000);
+            motorcycles[2] = Motorcycle.CreateMotorcycle(3, "Opel", "L1", 2017, 10_000);
+            motorcycles[3] = Motorcycle.CreateMotorcycle(4, "Minsk", "KK", 2013, 25_000);
+            motorcycles[4] = Motorcycle.CreateMotorcycle(5, "Mercsedes", "GT", 2019, 5_000);
+
+            Motorcycle.GetMotorcycles(motorcycles[4]);
+            Motorcycle.UpdateMotorcycle(motorcycles[4], 5_555);
+            Motorcycle.GetMotorcycles(motorcycles[4]);
+
+            Motorcycle.DeleteMotorcycle(ref motorcycles[0]);
+            Motorcycle.GetMotorcycles(motorcycles[0]);
+
+            Motorcycle.GetMotorcycleById(motorcycles, 2);
+        }
+    }
+}
